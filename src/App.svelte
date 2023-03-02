9f849e683d1bddd93996b30fd2c33887614a6f4e
Zen <muhzaini30@gmail.com>
2023-03-03 01:33:19 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Router from "svelte-spa-router";
+  // @ts-ignore
+  import routes from "virtual:generated-pages-svelte";
+  import "@unocss/reset/tailwind.css";
+  import "warna-tailwind/warna.css";
+</script>
+
+<Router {routes} />
+
+<style>
+  :global(*) {
+    word-wrap: break-word;
+  }
+  /* chrome extension */
+  /* :global(body) {
+    width: 350px;
+    font-size: 16px;
+  } */
+</style>
